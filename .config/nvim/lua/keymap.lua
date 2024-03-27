0bd336eaa0e85874395983cee01aa95e636921c2
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-03-27 14:49:00 -0700

    bug: fixing <number>k move up rather then move down bug

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -15,4 +15,4 @@ wk.register({
 }, { prefix = "<leader>" })
 
 keymap.set("n", "j", [[v:count?'j':'gj']], { noremap = true, expr = true })
-keymap.set("n", "k", [[v:count?'j':'gk']], { noremap = true, expr = true })
+keymap.set("n", "k", [[v:count?'k':'gk']], { noremap = true, expr = true })
