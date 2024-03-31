79adcf76390f7c329b0325335a9a937aad8f118e
Jackson Chen <90215880+Sma1lboy@users.noreply.github.com>
2024-03-31 01:11:42 -0700

    bug: fixing search cannot go next problems, also adding sign for breakpoint while debugging

--- .config/nvim/lua/p-dap.lua
+++ .config/nvim/lua/p-dap.lua
@@ -5,16 +5,20 @@ dapui.setup()
 -- keymaps
 local wk = require("which-key")
 wk.register({
+
   D = {
-    "<Cmd>lua require('dapui').toggle()<Cr>",
-    "Debugger",
+    D = {
+      "<Cmd>lua require('dapui').toggle()<Cr>",
+      "Debugger",
+    },
   },
 }, { prefix = "<leader>" })
 wk.register({
   n = {
-    n = {
+    b = {
       "<Cmd>lua require('dap').toggle_breakpoint()<Cr>",
       "Toggle Breakpoints",
+      mode = "n",
     },
   },
 })
@@ -32,3 +36,52 @@ end
 dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
+
+---breakpoint style
+local set_namespace = vim.api.nvim__set_hl_ns or vim.api.nvim_set_hl_ns
+local namespace = vim.api.nvim_create_namespace("dap-hlng")
+vim.api.nvim_set_hl(
+  namespace,
+  "DapBreakpoint",
+  { fg = "#eaeaeb", bg = "#ffffff" }
+)
+vim.api.nvim_set_hl(
+  namespace,
+  "DapLogPoint",
+  { fg = "#eaeaeb", bg = "#ffffff" }
+)
+vim.api.nvim_set_hl(namespace, "DapStopped", { fg = "#eaeaeb", bg = "#ffffff" })
+
+vim.fn.sign_define("DapBreakpoint", {
+  text = "🔴",
+  texthl = "DapBreakpoint",
+  linehl = "DapBreakpoint",
+  numhl = "DapBreakpoint",
+})
+vim.fn.sign_define("DapBreakpointCondition", {
+  text = "ﳁ",
+  texthl = "DapBreakpoint",
+  linehl = "DapBreakpoint",
+  numhl = "DapBreakpoint",
+})
+vim.fn.sign_define("DapBreakpointRejected", {
+  text = "",
+  texthl = "DapBreakpoint",
+  linehl = "DapBreakpoint",
+  numhl = "DapBreakpoint",
+})
+vim.fn.sign_define("DapLogPoint", {
+  text = "",
+  texthl = "DapLogPoint",
+  linehl = "DapLogPoint",
+  numhl = "DapLogPoint",
+})
+vim.fn.sign_define(
+  "DapStopped",
+  {
+    text = "",
+    texthl = "DapStopped",
+    linehl = "DapStopped",
+    numhl = "DapStopped",
+  }
+)
