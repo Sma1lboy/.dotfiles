63d882d1de4fed0773153e99047b5d704e62a97a
Sma1lboy <541898146chen@gmail.com>
2025-07-31 23:14:31 -0400

    chore: add Avante.nvim plugin with configuration and dependencies

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -289,6 +289,87 @@ require("lazy").setup({
       "rcarriga/nvim-notify",
     },
   },
+  {
+    "yetone/avante.nvim",
+    build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = "openai",
+      providers = {
+        openai = {
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "anthropic/claude-sonnet-4",
+          api_key_name = "OPENROUTER_API_KEY",
+          timeout = 30000,
+          context_window = 200000,
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 50000,
+            transforms = { "middle-out" },
+          },
+        },
+      },
+      mappings = {
+        ask = "<leader>aa",
+        new_ask = "<leader>an",
+        edit = "<leader>ae",
+        refresh = "<leader>ar",
+        focus = "<leader>af",
+        toggle = {
+          default = "<leader>l",
+        },
+        submit = {
+          normal = "<CR>", -- Enter in normal mode
+          insert = "<CR>", -- Enter in insert mode
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+      "stevearc/dressing.nvim",        -- for input provider dressing
+      "folke/snacks.nvim",             -- for input provider snacks
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",        -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
